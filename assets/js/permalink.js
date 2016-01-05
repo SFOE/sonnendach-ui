214e792774f5b8d5a16a7ac80a549809c06de5bc
oterral <olivier.terral@swisstopo.ch>
2016-01-05 10:54:34 +0100

    Add translations and permalink reader

---
+++ assets/js/permalink.js
@@ -0,0 +1,36 @@
+var addPermalink = function() {
+  var tryDecodeURIComponent = function(value) {
+    try {
+      return decodeURIComponent(value);
+    } catch (e) {
+    // Ignore any invalid uri component
+    }
+  };
+
+  var parseKeyValue = function(keysValues) {
+    var obj = {}; 
+    var kv;
+    var key;
+    var arr = (keysValues || '').split('&');
+    for (var i = 0; i < arr.length; i++) {
+      var keyValue = arr[i];
+      if (keyValue) {
+        kv = keyValue.split('=');
+        key = tryDecodeURIComponent(kv[0]);
+        if (key && kv[1]) {
+          obj[key] = tryDecodeURIComponent(kv[1]);
+        }    
+      }
+    }
+    return obj;
+  };
+
+  loc = window.location;
+  var res = parseKeyValue(loc.search.substring(1));
+  var lang =  (res.lang || '').split(',');
+
+  var url = {
+    lang: (lang[0].length) ? lang : undefined, 
+  }; 
+  return url;
+};
