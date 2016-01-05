f38af7981231811ffbcc37fd179a3a65d6474ab5
cype <martin@cube7.ch>
2016-01-05 19:24:35 +0100

    New translation added
    to understand how it works.

--- assets/js/translations.js
+++ assets/js/translations.js
@@ -1,6 +1,6 @@
 var sdTranslations = {
   mainTitle: {
-    de: 'Wie viel <strong>Strom</strong> und <strong>W&auml;rme</strong>' +
+    de: 'Wie viel <strong>Strom</strong> und <strong>W&auml;rme</strong> ' +
         'aus Sonnenenergie kann ich auf meinem Dach produzieren?',
     fr: 'Combien d\'<trong>energie</strong> je peux produire sur  mon toit'
   },
@@ -11,5 +11,9 @@ var sdTranslations = {
   locateMe: {
     de: 'Lokalisiere mich',
     fr: 'Localisez moi'
+  },
+  noRoofFound: {
+    de: 'Momentan decken wir einen Drittel der Schweiz ab. Wir f&uuml;gen halbj&auml;hrlich neue D&auml;cher hinzu. Anfang 2018 ist die ganze Schweiz abgedeckt.',
+    fr: 'french no roof found'
   }
 };
