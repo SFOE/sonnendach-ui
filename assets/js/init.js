82e300ee5581da790cb43aa9f1bf983f3c4e6ef8
oterral <olivier.terral@swisstopo.ch>
2016-01-05 17:06:37 +0100

    Put the translations in single file

--- assets/js/init.js
+++ assets/js/init.js
@@ -69,25 +69,10 @@ var init = function() {
   var body = $(document.body);
   var locationBt = $('#location');
   var markerElt = $('<div class="marker ga-crosshair"></div>');
-  var t = {
-    mainTitle: {
-      de: 'Wie viel <strong>Strom</strong> und <strong>W&auml;rme</strong>' +
-          ' aus Sonnenenergie kann ich auf meinem Dach produzieren?',
-      fr: 'Combien d\'<trong>energie</strong> je peux produire sur  mon toit'
-    },
-    subTitle: {
-      de: 'Finden Sie es heraus mit dem Solarkataster Schweiz.',
-      fr: 'Decouvrez le cadastre solaire Suisse.'
-    },
-    locateMe: {
-      de: 'Lokalisiere mich',
-      fr: 'Localisez moi'
-    }
-  };
   var permalink = addPermalink();
   var translator = body.translate({
     lang: (langs.indexOf(permalink.lang) != -1) ? permalink.lang : langs[0],
-    t: t
+    t: sdTranslations // Object defined in tranlations.js
   });
 
   // Create map
