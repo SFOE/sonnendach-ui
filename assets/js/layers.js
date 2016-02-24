eb9b0f39424ec3d229f394d2d978e5045d321678
Gilbert Jeiziner <gjn@space.ch>
2016-02-24 09:19:32 +0100

    Use layers Config service to get layers configuration

--- assets/js/layers.js
+++ assets/js/layers.js
@@ -2,93 +2,11 @@
  * Get the layers configuartion
  * 
  * @method
- * @return {Object}
+ * @return {Promise}
  */
 var getLayersConfig = function(lang) {
-  return {
-    "ch.swisstopo.swissimage": {
-      "attribution": "CNES, Spot Image, swisstopo, NPOC",
-      "background": true,
-      "searchable": false,
-      "format": "jpeg",
-      "hasLegend": false,
-      "serverLayerName": "ch.swisstopo.swissimage",
-      "selectbyrectangle": false,
-      "attributionUrl": "http://www.swisstopo.admin.ch/internet/swisstopo/" + lang + "/home.html",
-      "timeBehaviour": "last",
-      "tooltip": false,
-      "label": "SWISSIMAGE",
-      "highlightable": true,
-      "chargeable": true,
-      "timestamps": [
-        "20151231",
-        "20140620",
-        "20131107",
-        "20130916",
-        "20130422",
-        "20120809",
-        "20120225",
-        "20110914",
-        "20110228"
-      ],
-      "topics": "api,swissmaponline",
-      "resolutions": [
-        4000,
-        3750,
-        3500,
-        3250,
-        3000,
-        2750,
-        2500,
-        2250,
-        2000,
-        1750,
-        1500,
-        1250,
-        1000,
-        750,
-        650,
-        500,
-        250,
-        100,
-        50,
-        20,
-        10,
-        5,
-        2.5,
-        2,
-        1.5,
-        1,
-        0.5,
-        0.25
-      ],
-      "type": "wmts",
-      "timeEnabled": false,
-      "queryable": false
-    },
-    "ch.bfe.solarenergie-eignung-daecher": {
-      "queryableAttributes": ["building_id","df_uid"],
-      "attribution": "BFE",
-      "background":false,
-      "searchable":false,
-      "format":"png",
-      "hasLegend":true,
-      "serverLayerName": "ch.bfe.solarenergie-eignung-daecher",
-      "selectbyrectangle":true,
-      "attributionUrl":"http://www.bfe.admin.ch/index.html?lang=de",
-      "timeBehaviour":"last",
-      "tooltip":true,
-      "label":"Solarenergie: Eignung D\u00e4cher",
-      "highlightable":true,
-      "chargeable":false,
-      "timestamps":["20160218"],
-      "topics":"api,dev,ech,energie,inspire,swissmaponline,wms-bgdi_prod",
-      "resolutions": [4000,3750,3500,3250,3000,2750,2500,2250,2000,1750,1500,1250,1000,750,650,500,250,100,50,20,10,5,2.5,2,1.5,1,0.5],
-      "type":"wmts",
-      "timeEnabled":false,
-      "queryable":true
-    }
-  };
+  var url = API3_URL + '/rest/services/api/MapServer/layersConfig?lang=' + lang;
+  return $.getJSON(url);
 };
 
 /**
